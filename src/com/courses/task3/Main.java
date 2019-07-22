8c730860d82af39097918e160351860c0c5d9bf0
Bogdan <krabog@gmail.com>
2019-07-22 13:07:39 +0300

    start task3

---
+++ src/com/courses/task3/Main.java
@@ -0,0 +1,7 @@
+package com.courses.task3;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/courses/task3/view/TextConstant.java
@@ -0,0 +1,8 @@
+package com.courses.task3.view;
+
+public interface TextConstant {
+    String INPUT_STRING_DATA = "input.string.data";
+    String FIRST_NAME = "input.first.name.data";
+    String WRONG_INPUT = "input.wrong.data";
+    String LOGIN_DATA = "input.login.data";
+}
